2dbe959794320c9b48671d598c5045e259147d6b
Caskia <liubai61@163.com>
2017-12-20 17:54:05 +0800

    add jane asp net core

---
+++ src/Jane.AspNetCore/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Jane.Dependency;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Jane.Configurations
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceProvider AddJane(this IServiceCollection services)
+        {
+            if (ObjectContainer.Current is IContainer)
+            {
+                return new AutofacServiceProvider(ObjectContainer.Current as IContainer);
+            }
+            else
+            {
+                throw new Exception("Current container not support!");
+            }
+        }
+    }
+}
