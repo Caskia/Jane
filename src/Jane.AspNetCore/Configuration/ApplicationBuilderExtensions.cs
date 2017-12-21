30549b9063a9dd05d6c40b0f6fd4a92fe3226ee0
Caskia <liubai61@163.com>
2017-12-21 10:13:19 +0800

    fix referenece

--- src/Jane.AspNetCore/Configuration/ApplicationBuilderExtensions.cs
+++ src/Jane.AspNetCore/Configuration/ApplicationBuilderExtensions.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Jane.AspNetCore.Configuration
+namespace Jane.Configurations
 {
     public static class ApplicationBuilderExtensions
     {
