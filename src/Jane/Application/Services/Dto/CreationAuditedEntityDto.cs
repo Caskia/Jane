1b4940d57c3a3f230542f9616f7e68091095b188
Caskia <liubai61@163.com>
2018-01-03 16:14:47 +0800

    add dto

---
+++ src/Jane/Application/Services/Dto/CreationAuditedEntityDto.cs
@@ -0,0 +1,40 @@
+using Jane.Entities.Auditing;
+using Jane.Timing;
+using System;
+
+namespace Jane.Application.Services.Dto
+{
+    /// <summary>
+    ///  A shortcut of <see cref="CreationAuditedEntityDto"/> for most used primary key type (<see cref="int"/>).
+    /// </summary>
+    [Serializable]
+    public abstract class CreationAuditedEntityDto : CreationAuditedEntityDto<int>
+    {
+    }
+
+    /// <summary>
+    /// This class can be inherited for simple Dto objects those are used for entities implement <see cref="ICreationAudited"/> interface.
+    /// </summary>
+    /// <typeparam name="TPrimaryKey">Type of primary key</typeparam>
+    [Serializable]
+    public abstract class CreationAuditedEntityDto<TPrimaryKey> : EntityDto<TPrimaryKey>, ICreationAudited
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        protected CreationAuditedEntityDto()
+        {
+            CreationTime = Clock.Now;
+        }
+
+        /// <summary>
+        /// Creation date of this entity.
+        /// </summary>
+        public DateTime CreationTime { get; set; }
+
+        /// <summary>
+        /// Creator user's id for this entity.
+        /// </summary>
+        public long? CreatorUserId { get; set; }
+    }
+}
