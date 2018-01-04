7c0a018b8a16e87096bd1e88b3d9afd97dea76fb
Caskia <liubai61@163.com>
2018-01-04 10:55:48 +0800

    push service fix

--- src/Jane/PushNotifications/NullPushNotificationService.cs
+++ src/Jane/PushNotifications/NullPushNotificationService.cs
@@ -5,36 +5,36 @@ namespace Jane.PushNotifications
 {
     public class NullPushNotificationService : IPushNotificationService
     {
-        private readonly ILogger _logger;
+        private readonly ILogger Logger;
 
         public NullPushNotificationService(
-               ILogger logger
+               ILoggerFactory loggerFactory
                )
         {
-            _logger = logger;
+            Logger = loggerFactory.Create(typeof(NullPushNotificationService));
         }
 
         public Task<bool> SendToDeviceAsync(string deviceToken, string title, string body, string clickAction, dynamic data)
         {
-            _logger.Warn("USING NullPushNotificationService!");
-            _logger.Debug("SendToDeviceAsync:");
-            _logger.Debug($"DeviceToken: {deviceToken}");
-            _logger.Debug($"Title: {title}");
-            _logger.Debug($"Body: {body}");
-            _logger.Debug($"ClickAction: {clickAction}");
-            _logger.Debug($"Data: {data}");
+            Logger.Warn("USING NullPushNotificationService!");
+            Logger.Debug("SendToDeviceAsync:");
+            Logger.Debug($"DeviceToken: {deviceToken}");
+            Logger.Debug($"Title: {title}");
+            Logger.Debug($"Body: {body}");
+            Logger.Debug($"ClickAction: {clickAction}");
+            Logger.Debug($"Data: {data}");
             return Task.FromResult(false);
         }
 
         public Task<bool> SendToTopicAsync(string topic, string title, string body, string clickAction, dynamic data)
         {
-            _logger.Warn("USING NullPushNotificationService!");
-            _logger.Debug("SendToTopicAsync:");
-            _logger.Debug($"Topic: {topic}");
-            _logger.Debug($"Title: {title}");
-            _logger.Debug($"Body: {body}");
-            _logger.Debug($"ClickAction: {clickAction}");
-            _logger.Debug($"Data: {data}");
+            Logger.Warn("USING NullPushNotificationService!");
+            Logger.Debug("SendToTopicAsync:");
+            Logger.Debug($"Topic: {topic}");
+            Logger.Debug($"Title: {title}");
+            Logger.Debug($"Body: {body}");
+            Logger.Debug($"ClickAction: {clickAction}");
+            Logger.Debug($"Data: {data}");
             return Task.FromResult(false);
         }
     }
