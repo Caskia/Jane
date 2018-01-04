d596011e1d7f806ca6626b31331e0ac446638e80
Caskia <liubai61@163.com>
2018-01-04 12:49:50 +0800

    add socket utils

---
+++ src/Jane/Utils/SocketUtils.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace Jane.Utils
+{
+    public class SocketUtils
+    {
+        public static IPAddress GetLocalIPV4()
+        {
+            var networkTypes = new List<NetworkInterfaceType>()
+            {
+                NetworkInterfaceType.Ethernet,
+                NetworkInterfaceType.Wireless80211
+            };
+            var hasGatewayNetworkInterfaces = NetworkInterface.GetAllNetworkInterfaces()
+                .Where(ni => ni.GetIPProperties().GatewayAddresses.Count > 0);
+            var ethernetNetworkInterfaces = hasGatewayNetworkInterfaces
+                .Where(ni => networkTypes.Contains(ni.NetworkInterfaceType));
+
+            return ethernetNetworkInterfaces
+                .Select(ni => ni.GetIPProperties().UnicastAddresses.First(x => x.Address.AddressFamily == AddressFamily.InterNetwork).Address)
+                .First();
+        }
+    }
+}
