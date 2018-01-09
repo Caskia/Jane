153e22163277ac07ff42b059475b63b2105b5664
Caskia <liubai61@163.com>
2018-01-09 17:55:44 +0800

    add enode aspnet core

---
+++ src/Jane.ENode.AspNetCore/Configurations/ConfigurationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Jane.AspNetCore.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using ECommonConfiguration = ECommon.Configurations.Configuration;
+using EENode = ENode;
+
+namespace Jane.Configurations
+{
+    /// <summary>
+    /// configuration class AspNetCore extensions.
+    /// </summary>
+    public static class ConfigurationExtensions
+    {
+        public static ECommonConfiguration UseECommonAspNetCore(
+            this ECommonConfiguration configuration,
+            IServiceCollection services,
+            out IServiceProvider serviceProvider
+            )
+        {
+            configuration.SetDefault<ILogger, JaneMsLoggerAdapter>();
+
+            serviceProvider = services.AddECommon();
+
+            return configuration;
+        }
+
+        public static EENode.Configurations.ENodeConfiguration UseENodeAspNetCore(
+            this EENode.Configurations.ENodeConfiguration configuration,
+            IServiceCollection services,
+            out IServiceProvider serviceProvider
+            )
+        {
+            configuration.GetCommonConfiguration()
+                  .UseECommonAspNetCore(services, out serviceProvider);
+            return configuration;
+        }
+    }
+}
---
+++ src/Jane.ENode.AspNetCore/Configurations/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Autofac.Extensions.DependencyInjection;
+using ECommon.Autofac;
+using ECommon.Components;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Jane.Configurations
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceProvider AddECommon(this IServiceCollection services)
+        {
+            if (ObjectContainer.Current is AutofacObjectContainer && Dependency.ObjectContainer.Current is Autofac.AutofacObjectContainer)
+            {
+                var ecommonObjectContainer = ObjectContainer.Current as AutofacObjectContainer;
+
+                ecommonObjectContainer.ContainerBuilder.Populate(services);
+                ecommonObjectContainer.Build();
+
+                //regist jane object container
+                Dependency.ObjectContainer.SetContainer(new Autofac.AutofacObjectContainer(ecommonObjectContainer.ContainerBuilder));
+
+                var janeObjectContainer = Dependency.ObjectContainer.Current as Autofac.AutofacObjectContainer;
+                janeObjectContainer.SetContainer(ecommonObjectContainer.Container);
+
+                return new AutofacServiceProvider(ecommonObjectContainer.Container);
+            }
+            else
+            {
+                throw new BaseException("Current container not support!");
+            }
+        }
+    }
+}
