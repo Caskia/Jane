3e0233bc37a89a74edaad8f1704b3e856a87d7fd
Caskia <liubai61@163.com>
2018-01-17 11:26:17 +0800

    name server use hostname

--- src/Jane.ENode/ENode/EQueueConfiguration.cs
+++ src/Jane.ENode/ENode/EQueueConfiguration.cs
@@ -86,7 +86,7 @@ namespace Jane.ENode
                     foreach (var address in addressList)
                     {
                         var array = address.Split(new string[] { ":" }, StringSplitOptions.RemoveEmptyEntries);
-                        var endpoint = new IPEndPoint(IPAddress.Parse(array[0]), int.Parse(array[1]));
+                        var endpoint = SocketUtils.GetIPEndPointFromHostName(array[0], int.Parse(array[1]));
                         ipEndPoints.Add(endpoint);
                     }
                 }
