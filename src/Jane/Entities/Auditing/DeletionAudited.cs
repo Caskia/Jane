c1ec76dfe9d0b764349322927a040c5a71fe39c7
Caskia <liubai61@163.com>
2018-01-23 15:46:10 +0800

    add deletion audited, modification audited

---
+++ src/Jane/Entities/Auditing/DeletionAudited.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Jane.Entities.Auditing
+{
+    /// <summary>
+    /// This class can be used to simplify implementing <see cref="IDeletionAudited"/>.
+    /// </summary>
+    [Serializable]
+    public class DeletionAudited : IDeletionAudited
+    {
+        /// <summary>
+        /// Which user deleted this entity?
+        /// </summary>
+        public virtual long? DeleterUserId { get; set; }
+
+        /// <summary>
+        /// Deletion time of this entity.
+        /// </summary>
+        public virtual DateTime? DeletionTime { get; set; }
+
+        /// <summary>
+        /// Is this entity Deleted?
+        /// </summary>
+        public virtual bool IsDeleted { get; set; }
+    }
+}
---
+++ src/Jane/Entities/Auditing/ModificationAudited.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jane.Entities.Auditing
+{
+    /// <summary>
+    /// This class can be used to simplify implementing <see cref="IModificationAudited"/>.
+    /// </summary>
+    [Serializable]
+    public class ModificationAudited : IModificationAudited
+    {
+        /// <summary>
+        /// Last modification date of this entity.
+        /// </summary>
+        public virtual DateTime? LastModificationTime { get; set; }
+
+        /// <summary>
+        /// Last modifier user of this entity.
+        /// </summary>
+        public virtual long? LastModifierUserId { get; set; }
+    }
+}
