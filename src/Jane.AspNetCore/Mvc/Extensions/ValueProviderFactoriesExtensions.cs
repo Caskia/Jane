7add471d38a0323ad6750c80dc7c6cb1eea4b60c
Caskia <liubai61@163.com>
2018-01-31 11:18:35 +0800

    add delimited querystring

---
+++ src/Jane.AspNetCore/Mvc/Extensions/ValueProviderFactoriesExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Jane.AspNetCore.Mvc.Providers;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jane.AspNetCore.Mvc.Extensions
+{
+    public static class ValueProviderFactoriesExtensions
+    {
+        public static void AddDelimitedValueProviderFactory(
+            this IList<IValueProviderFactory> valueProviderFactories,
+            params char[] delimiters)
+        {
+            var queryStringValueProviderFactory = valueProviderFactories
+                .OfType<QueryStringValueProviderFactory>()
+                .FirstOrDefault();
+            if (queryStringValueProviderFactory == null)
+            {
+                valueProviderFactories.Insert(
+                    0,
+                    new DelimitedQueryStringValueProviderFactory(delimiters));
+            }
+            else
+            {
+                valueProviderFactories.Insert(
+                    valueProviderFactories.IndexOf(queryStringValueProviderFactory),
+                    new DelimitedQueryStringValueProviderFactory(delimiters));
+                valueProviderFactories.Remove(queryStringValueProviderFactory);
+            }
+        }
+    }
+}
---
+++ src/Jane.AspNetCore/Mvc/Providers/DelimitedQueryStringValueProviderFactory.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Jane.AspNetCore.Mvc.Providers
+{
+    /// <summary>
+    /// A <see cref="IValueProviderFactory"/> that creates <see cref="IValueProvider"/> instances that
+    /// read optionally delimited values from the request query-string.
+    /// </summary>
+    public class DelimitedQueryStringValueProviderFactory : IValueProviderFactory
+    {
+        private static readonly char[] DefaultDelimiters = new char[] { ',' };
+        private readonly char[] delimiters;
+
+        public DelimitedQueryStringValueProviderFactory()
+            : this(DefaultDelimiters)
+        {
+        }
+
+        public DelimitedQueryStringValueProviderFactory(params char[] delimiters)
+        {
+            if (delimiters == null || delimiters.Length == 0)
+            {
+                this.delimiters = DefaultDelimiters;
+            }
+            else
+            {
+                this.delimiters = delimiters;
+            }
+        }
+
+        public Task CreateValueProviderAsync(ValueProviderFactoryContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var valueProvider = new DelimitedQueryStringValueProvider(
+                BindingSource.Query,
+                context.ActionContext.HttpContext.Request.Query,
+                CultureInfo.InvariantCulture,
+                this.delimiters);
+
+            context.ValueProviders.Add(valueProvider);
+
+            return Task.CompletedTask;
+        }
+    }
+}
