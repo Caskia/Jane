9b9a6aa739b6ccc98a537d2899ce7b103404d2b2
Caskia <liubai123@hotmail.com>
2018-10-07 15:22:19 +0800

    add json config

---
+++ src/Jane/Json/JsonConfig.cs
@@ -0,0 +1,48 @@
+﻿using Jane.Json.Converters;
+using Jane.Json.Formatters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Jane.Json
+{
+    public static class JsonConfig
+    {
+        private static JsonSerializer _serializer;
+
+        private static JsonSerializerSettings _serializerSettings;
+
+        public static JsonSerializer DefaultJsonSerializer
+        {
+            get
+            {
+                if (_serializer == null)
+                {
+                    _serializer = new JsonSerializer();
+                    _serializer.ContractResolver = new CustomPropertyNamesContractResolver();
+                    _serializer.Converters.Add(new JaneDateTimeConverter());
+                    _serializer.Converters.Add(new LongConverter());
+                    _serializer.Converters.Add(new StringEnumConverter());
+                }
+
+                return _serializer;
+            }
+        }
+
+        public static JsonSerializerSettings DefaultJsonSerializerSettings
+        {
+            get
+            {
+                if (_serializerSettings == null)
+                {
+                    _serializerSettings = new JsonSerializerSettings();
+                    _serializerSettings.ContractResolver = new CustomPropertyNamesContractResolver();
+                    _serializerSettings.Converters.Add(new JaneDateTimeConverter());
+                    _serializerSettings.Converters.Add(new LongConverter());
+                    _serializerSettings.Converters.Add(new StringEnumConverter());
+                }
+
+                return _serializerSettings;
+            }
+        }
+    }
+}
