d6160a175056b43b270ee9850ab91f3cf4b13250
Caskia <liubai61@163.com>
2018-10-10 21:15:06 +0800

    add datetime extensions

--- src/Jane/Extensions/DateTimeExtensions.cs
+++ src/Jane/Extensions/DateTimeExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace Jane.Extensions
+﻿using System;
+using System.Linq;
+
+namespace Jane.Extensions
 {
     /// <summary>
     /// Extension methods for <see cref="DateTime"/>.
@@ -6,5 +9,88 @@
     public static class DateTimeExtensions
     {
         //...
+        private static DateTime _epochStartDateTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static long ConvertDateTimeToEpoch(this DateTime datetime)
+        {
+            if (datetime < _epochStartDateTime) return 0;
+
+            return Convert.ToInt64((datetime.ToUniversalTime() - _epochStartDateTime).TotalSeconds);
+        }
+
+        public static DateTime ConvertEpochToDateTime(long seconds)
+        {
+            return _epochStartDateTime.AddSeconds(seconds);
+        }
+
+        public static DateTimeOffset CutOffHours(this DateTimeOffset dateTimeOffset)
+        {
+            return new DateTimeOffset(dateTimeOffset.Ticks - (dateTimeOffset.Ticks % TimeSpan.TicksPerDay), dateTimeOffset.Offset);
+        }
+
+        public static DateTime CutOffHours(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Ticks - (dateTime.Ticks % TimeSpan.TicksPerDay), dateTime.Kind);
+        }
+
+        public static DateTimeOffset CutOffSeconds(this DateTimeOffset dateTimeOffset)
+        {
+            return new DateTimeOffset(dateTimeOffset.Ticks - (dateTimeOffset.Ticks % TimeSpan.TicksPerMinute), dateTimeOffset.Offset);
+        }
+
+        public static DateTime CutOffSeconds(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Ticks - (dateTime.Ticks % TimeSpan.TicksPerMinute), dateTime.Kind);
+        }
+
+        public static bool IsUtcDst(this DateTime dateTimeUtc, TimeZoneInfo timeZoneInfo)
+        {
+            return timeZoneInfo.IsDaylightSavingTime(dateTimeUtc.AddHours(timeZoneInfo.BaseUtcOffset.TotalHours));
+        }
+
+        public static bool IsUtcDst(this DateTime dateTimeUtc, string timeZoneId)
+        {
+            if (string.IsNullOrEmpty(timeZoneId))
+            {
+                return false;
+            }
+            var timeZoneInfo = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(p => p.Id == timeZoneId);
+            if (timeZoneInfo == null)
+            {
+                return false;
+            }
+            return dateTimeUtc.IsUtcDst(timeZoneInfo);
+        }
+
+        public static string ToDateSuffix(this DateTimeOffset dateTimeOffset)
+        {
+            return dateTimeOffset.Day.ToOccurrenceSuffix();
+        }
+
+        public static string ToDateSuffix(this DateTime dateTime)
+        {
+            return dateTime.Day.ToOccurrenceSuffix();
+        }
+
+        public static DateTime ToLocalTime(this DateTime utcTime, string windowsTimeZoneId)
+        {
+            var tzi = TimeZoneInfo.FindSystemTimeZoneById(windowsTimeZoneId);
+            var tziLocal = TimeZoneInfo.ConvertTimeFromUtc(utcTime, tzi);
+            return tziLocal;
+        }
+
+        public static DateTimeOffset ToLocalTime(this DateTimeOffset utcTime, string windowsTimeZoneId)
+        {
+            var tzi = TimeZoneInfo.FindSystemTimeZoneById(windowsTimeZoneId);
+            var tziLocal = TimeZoneInfo.ConvertTime(utcTime, tzi);
+            return tziLocal;
+        }
+
+        public static DateTime ToUtcTime(this DateTime localTime, string windowsTimeZoneId)
+        {
+            var tzi = TimeZoneInfo.FindSystemTimeZoneById(windowsTimeZoneId);
+            var tziLocal = TimeZoneInfo.ConvertTimeToUtc(localTime, tzi);
+            return tziLocal;
+        }
     }
 }
--- src/Jane/Extensions/StringExtensions.cs
+++ src/Jane/Extensions/StringExtensions.cs
@@ -389,6 +389,31 @@ namespace Jane.Extensions
             }
         }
 
+        public static string ToOccurrenceSuffix(this int integer)
+        {
+            switch (integer % 100)
+            {
+                case 11:
+                case 12:
+                case 13:
+                    return "th";
+            }
+            switch (integer % 10)
+            {
+                case 1:
+                    return "st";
+
+                case 2:
+                    return "nd";
+
+                case 3:
+                    return "rd";
+
+                default:
+                    return "th";
+            }
+        }
+
         /// <summary>
         /// Converts camelCase string to PascalCase string.
         /// </summary>
