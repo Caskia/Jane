972d2663e409b9c893d330fa10f9079ef7e1cdb9
Caskia <liubai61@163.com>
2018-10-10 21:30:20 +0800

    remove time methods

--- src/Jane/Extensions/DateTimeExtensions.cs
+++ src/Jane/Extensions/DateTimeExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace Jane.Extensions
 {
@@ -8,21 +7,6 @@ namespace Jane.Extensions
     /// </summary>
     public static class DateTimeExtensions
     {
-        //...
-        private static DateTime _epochStartDateTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
-        public static long ConvertDateTimeToEpoch(this DateTime datetime)
-        {
-            if (datetime < _epochStartDateTime) return 0;
-
-            return Convert.ToInt64((datetime.ToUniversalTime() - _epochStartDateTime).TotalSeconds);
-        }
-
-        public static DateTime ConvertEpochToDateTime(long seconds)
-        {
-            return _epochStartDateTime.AddSeconds(seconds);
-        }
-
         public static DateTimeOffset CutOffHours(this DateTimeOffset dateTimeOffset)
         {
             return new DateTimeOffset(dateTimeOffset.Ticks - (dateTimeOffset.Ticks % TimeSpan.TicksPerDay), dateTimeOffset.Offset);
@@ -43,25 +27,6 @@ namespace Jane.Extensions
             return new DateTime(dateTime.Ticks - (dateTime.Ticks % TimeSpan.TicksPerMinute), dateTime.Kind);
         }
 
-        public static bool IsUtcDst(this DateTime dateTimeUtc, TimeZoneInfo timeZoneInfo)
-        {
-            return timeZoneInfo.IsDaylightSavingTime(dateTimeUtc.AddHours(timeZoneInfo.BaseUtcOffset.TotalHours));
-        }
-
-        public static bool IsUtcDst(this DateTime dateTimeUtc, string timeZoneId)
-        {
-            if (string.IsNullOrEmpty(timeZoneId))
-            {
-                return false;
-            }
-            var timeZoneInfo = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(p => p.Id == timeZoneId);
-            if (timeZoneInfo == null)
-            {
-                return false;
-            }
-            return dateTimeUtc.IsUtcDst(timeZoneInfo);
-        }
-
         public static string ToDateSuffix(this DateTimeOffset dateTimeOffset)
         {
             return dateTimeOffset.Day.ToOccurrenceSuffix();
@@ -70,27 +35,6 @@ namespace Jane.Extensions
         public static string ToDateSuffix(this DateTime dateTime)
         {
             return dateTime.Day.ToOccurrenceSuffix();
-        }
-
-        public static DateTime ToLocalTime(this DateTime utcTime, string windowsTimeZoneId)
-        {
-            var tzi = TimeZoneInfo.FindSystemTimeZoneById(windowsTimeZoneId);
-            var tziLocal = TimeZoneInfo.ConvertTimeFromUtc(utcTime, tzi);
-            return tziLocal;
-        }
-
-        public static DateTimeOffset ToLocalTime(this DateTimeOffset utcTime, string windowsTimeZoneId)
-        {
-            var tzi = TimeZoneInfo.FindSystemTimeZoneById(windowsTimeZoneId);
-            var tziLocal = TimeZoneInfo.ConvertTime(utcTime, tzi);
-            return tziLocal;
-        }
-
-        public static DateTime ToUtcTime(this DateTime localTime, string windowsTimeZoneId)
-        {
-            var tzi = TimeZoneInfo.FindSystemTimeZoneById(windowsTimeZoneId);
-            var tziLocal = TimeZoneInfo.ConvertTimeToUtc(localTime, tzi);
-            return tziLocal;
         }
     }
 }
