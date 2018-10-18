29529cf2efdf26a49def536f50696ef636b01010
Caskia <liubai61@163.com>
2018-10-18 10:32:17 +0800

    add wait and retry async

--- src/Jane/Threading/TimerTaskFactory.cs
+++ src/Jane/Threading/TimerTaskFactory.cs
@@ -61,5 +61,59 @@ namespace Jane.Threading
 
             return taskCompletionSource.Task;
         }
+
+        /// <summary>
+        /// Starts a new task that will poll for a result using the specified function, and will be completed when it satisfied the specified condition.
+        /// </summary>
+        /// <typeparam name="T">The type of value that will be returned when the task completes.</typeparam>
+        /// <param name="getResult">Function that will be used for polling.</param>
+        /// <param name="isResultValid">Predicate that determines if the result is valid, or if it should continue polling</param>
+        /// <param name="pollInterval">Polling interval.</param>
+        /// <param name="timeout">The timeout interval.</param>
+        /// <returns>The result returned by the specified function, or <see langword="null"/> if the result is not valid and the task times out.</returns>
+        public static Task<T> StartNewAsync<T>(Func<Task<T>> getResult, Func<Task<T>, Task<bool>> isResultValid, TimeSpan pollInterval, TimeSpan timeout)
+        {
+            Timer timer = null;
+            TaskCompletionSource<T> taskCompletionSource = null;
+            DateTime expirationTime = DateTime.UtcNow.Add(timeout);
+
+            timer =
+                new Timer(async _ =>
+                {
+                    try
+                    {
+                        if (DateTime.UtcNow > expirationTime)
+                        {
+                            timer.Dispose();
+                            taskCompletionSource.SetResult(default(T));
+                            return;
+                        }
+
+                        var result = getResult();
+
+                        if (await isResultValid(result))
+                        {
+                            timer.Dispose();
+                            taskCompletionSource.SetResult(await result);
+                        }
+                        else
+                        {
+                            // try again
+                            timer.Change(pollInterval, DoNotRepeat);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        timer.Dispose();
+                        taskCompletionSource.SetException(e);
+                    }
+                });
+
+            taskCompletionSource = new TaskCompletionSource<T>(timer);
+
+            timer.Change(pollInterval, DoNotRepeat);
+
+            return taskCompletionSource.Task;
+        }
     }
 }
