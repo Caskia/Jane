66cd534acde8be3a9e3aa8feba33539f57a2fcc7
Caskia <liubai61@163.com>
2018-10-24 16:16:09 +0800

    using

--- src/Jane.MongoDb/Serializers/JObjectSerializer.cs
+++ src/Jane.MongoDb/Serializers/JObjectSerializer.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson.Serialization;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using Newtonsoft.Json.Linq;
 
@@ -14,7 +15,7 @@ namespace Jane.MongoDb.Serializers
 
         public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, JObject value)
         {
-            var bsonDocument = MongoDB.Bson.BsonDocument.Parse(value.ToString());
+            var bsonDocument = BsonDocument.Parse(value.ToString());
             BsonDocumentSerializer.Instance.Serialize(context, bsonDocument);
         }
     }
