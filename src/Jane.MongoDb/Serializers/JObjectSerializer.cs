504da0e2b85b76325b8cb6f49259f8ed4c08be68
Caskia <liubai61@163.com>
2018-10-24 16:15:45 +0800

    add jobject serializer

---
+++ src/Jane.MongoDb/Serializers/JObjectSerializer.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using Newtonsoft.Json.Linq;
+
+namespace Jane.MongoDb.Serializers
+{
+    public class JObjectSerializer : SerializerBase<JObject>
+    {
+        public override JObject Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            var bsonDocument = BsonDocumentSerializer.Instance.Deserialize(context);
+            return JObject.Parse(bsonDocument.ToString());
+        }
+
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, JObject value)
+        {
+            var bsonDocument = MongoDB.Bson.BsonDocument.Parse(value.ToString());
+            BsonDocumentSerializer.Instance.Serialize(context, bsonDocument);
+        }
+    }
+}
