d3be026955f08436ecb89e6579c1a4fb1fd25955
Caskia <liubai61@163.com>
2018-11-09 12:07:04 +0800

    add ProjectionEqualityComparer

---
+++ src/Jane/Utils/ProjectionEqualityComparer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jane.Utils
+{
+    public static class ProjectionEqualityComparer
+    {
+        public static ProjectionEqualityComparer<TSource, TKey> Create<TSource, TKey>(Func<TSource, TKey> projection)
+        {
+            return new ProjectionEqualityComparer<TSource, TKey>(projection);
+        }
+
+        public static ProjectionEqualityComparer<TSource, TKey> Create<TSource, TKey>(TSource ignored, Func<TSource, TKey> projection)
+        {
+            return new ProjectionEqualityComparer<TSource, TKey>(projection);
+        }
+    }
+
+    public static class ProjectionEqualityComparer<TSource>
+    {
+        public static ProjectionEqualityComparer<TSource, TKey> Create<TKey>(Func<TSource, TKey> projection)
+        {
+            return new ProjectionEqualityComparer<TSource, TKey>(projection);
+        }
+    }
+
+    public class ProjectionEqualityComparer<TSource, TKey>
+        : IEqualityComparer<TSource>
+    {
+        private readonly IEqualityComparer<TKey> comparer;
+        private readonly Func<TSource, TKey> projection;
+
+        public ProjectionEqualityComparer(Func<TSource, TKey> projection)
+            : this(projection, null)
+        {
+        }
+
+        public ProjectionEqualityComparer(Func<TSource, TKey> projection, IEqualityComparer<TKey> comparer)
+        {
+            this.comparer = comparer ?? EqualityComparer<TKey>.Default;
+            this.projection = projection ?? throw new ArgumentNullException(nameof(projection));
+        }
+
+        public bool Equals(TSource x, TSource y)
+        {
+            if (x == null && y == null)
+            {
+                return true;
+            }
+            if (x == null || y == null)
+            {
+                return false;
+            }
+            return comparer.Equals(projection(x), projection(y));
+        }
+
+        public int GetHashCode(TSource obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException("obj");
+            }
+            return comparer.GetHashCode(projection(obj));
+        }
+    }
+}
