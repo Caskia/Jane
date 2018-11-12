3b9ae92c8b039177c51aa15b0e9e930f2a86562b
Caskia <liubai123@hotmail.com>
2018-11-12 21:28:34 +0800

    add hangefire

---
+++ src/Jane.HangFire/Configurations/ConfigurationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace Jane.Configurations
+{
+    /// <summary>
+    /// configuration class hangefire extensions.
+    /// </summary>
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// use redis as the cache
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static Configuration UseHangeFire(this Configuration configuration)
+        {
+            var assemblies = new[]
+            {
+                Assembly.GetExecutingAssembly()
+            };
+            configuration.RegisterAssemblies(assemblies);
+
+            return configuration;
+        }
+    }
+}
