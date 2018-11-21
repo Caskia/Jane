ca9bdf6f3433a9e18cb5946d7b7cae5dfa2fdfa6
Caskia <liubai61@163.com>
2018-11-21 12:29:57 +0800

    add hangfire filter

---
+++ src/Jane.Hangfire/JaneHangfireAuthorizationFilter.cs
@@ -0,0 +1,31 @@
+﻿using Hangfire.Dashboard;
+using Jane.Dependency;
+using Jane.Runtime.Session;
+
+namespace Jane.Hangfire
+{
+    public class JaneHangfireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        private readonly string _requiredPermissionName;
+
+        public JaneHangfireAuthorizationFilter(string requiredPermissionName = null)
+        {
+            _requiredPermissionName = requiredPermissionName;
+        }
+
+        public bool Authorize(DashboardContext context)
+        {
+            if (!IsLoggedIn())
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsLoggedIn()
+        {
+            return ObjectContainer.Resolve<IJaneSession>().UserId.HasValue;
+        }
+    }
+}
