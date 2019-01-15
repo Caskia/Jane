fcd06636d544135b8c34c132068731860c433702
Caskia <liubai61@163.com>
2019-01-15 12:39:32 +0800

    add time stamp millisecond converter

--- src/Jane/Utils/DateTimeConverter.cs
+++ src/Jane/Utils/DateTimeConverter.cs
@@ -5,20 +5,36 @@ namespace Jane.Utils
 {
     public static class DateTimeConverter
     {
-        public static DateTime ConvertFromTimestampSeconds(double timestamp)
+        public static DateTime ConvertFromTimestampMilliseconds(double timestamp)
         {
-            DateTime converted = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            var converted = new DateTime(1970, 1, 1, 0, 0, 0, 0);
 
-            DateTime newDateTime = converted.AddSeconds(timestamp);
+            var newDateTime = converted.AddMilliseconds(timestamp);
 
             return newDateTime;
         }
 
+        public static DateTime ConvertFromTimestampSeconds(double timestamp)
+        {
+            var converted = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+
+            var newDateTime = converted.AddSeconds(timestamp);
+
+            return newDateTime;
+        }
+
+        public static double ConvertToTimestampMilliseconds(DateTime dateTimeUtc)
+        {
+            var span = dateTimeUtc - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+
+            return span.TotalMilliseconds;
+        }
+
         public static double ConvertToTimestampSeconds(DateTime dateTimeUtc)
         {
-            TimeSpan span = dateTimeUtc - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            var span = dateTimeUtc - new DateTime(1970, 1, 1, 0, 0, 0, 0);
 
-            return (double)span.TotalSeconds;
+            return Math.Ceiling(span.TotalSeconds);
         }
 
         public static TimeSpan ConvertToTimezoneOffset(string windowsTimeZoneId)
