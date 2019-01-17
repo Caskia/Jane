782987c22b6f2cf868e7225b0dc992b6cfc6a225
Caskia <liubai61@163.com>
2019-01-17 16:29:56 +0800

    add property cache

---
+++ src/Jane/Reflection/PropertyCache.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Jane.Reflection
+{
+    public class PropertyCache<T>
+    {
+        static PropertyCache()
+        {
+            Properties = typeof(T).GetProperties();
+        }
+
+        public static IEnumerable<PropertyInfo> Properties { get; }
+
+        public void ValidateProperties(IList<string> properties)
+        {
+            foreach (var @property in properties)
+            {
+                var prop = Properties.FirstOrDefault(item => item.Name == @property);
+                if (prop == null)
+                {
+                    throw new ArgumentException($"{nameof(@property)} is not a recognizable property.");
+                }
+            }
+        }
+    }
+}
