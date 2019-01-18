f5daf132f4ff939b37d3bf1b65079dd8b421f4a6
Caskia <liubai61@163.com>
2019-01-18 10:03:29 +0800

    add pascal key dictionary binder

---
+++ src/Jane.AspNetCore/Mvc/ModelBinding/PascalKeyDictionaryBinder.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jane.AspNetCore.Mvc.ModelBinding
+{
+    public class PascalKeyDictionaryBinder<TValue> : IModelBinder
+    {
+        public async Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            var streamReader = new StreamReader(bindingContext.HttpContext.Request.Body);
+            var json = await streamReader.ReadToEndAsync();
+            var dictionary = JsonConvert.DeserializeObject<IDictionary<string, TValue>>(json);
+            var model = dictionary.Select(keyValuePair =>
+            {
+                var keyStrs = keyValuePair.Key.Split('_').Select(s => char.ToUpper(s[0]) + s.Substring(1)).ToArray();
+                var key = string.Join("", keyStrs);
+                return new KeyValuePair<string, TValue>(key, keyValuePair.Value);
+            });
+
+            bindingContext.Result = ModelBindingResult.Success(model);
+        }
+    }
+}
