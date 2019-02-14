e4c6fc946b586166ef32969e52e307691bd11f31
Caskia <liubai61@163.com>
2019-02-14 10:28:01 +0800

    upgrade to aspnet core 2.2

--- src/Jane/Scheduling/IScheduleService.cs
+++ src/Jane/Scheduling/IScheduleService.cs
@@ -4,6 +4,13 @@ namespace Jane.Scheduling
 {
     public interface IScheduleService
     {
+        /// <summary>
+        /// start a task
+        /// </summary>
+        /// <param name="name">task name</param>
+        /// <param name="action">task</param>
+        /// <param name="dueTime">when the first event will be fired</param>
+        /// <param name="period">how often after that</param>
         void StartTask(string name, Action action, int dueTime, int period);
 
         void StopTask(string name);
