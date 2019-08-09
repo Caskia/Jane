ff65c4eb5ce7b09a13fee65bfa673794db700e52
Caskia <liubai61@163.com>
2019-08-09 10:04:12 +0800

    add object dictionary serializer

---
+++ src/Jane.MongoDb/Serializers/ObjectDictionarySerializer.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Options;
+using MongoDB.Bson.Serialization.Serializers;
+using System.Collections.Generic;
+
+namespace Jane.MongoDb.Serializers
+{
+    public class ObjectDictionarySerializer : DictionarySerializerBase<Dictionary<string, object>>
+    {
+        public ObjectDictionarySerializer()
+            : this(DictionaryRepresentation.Document)
+        {
+        }
+
+        public ObjectDictionarySerializer(DictionaryRepresentation dictionaryRepresentation)
+            : base(dictionaryRepresentation, new ObjectSerializer(), new SimpleObjectSerializer())
+        {
+        }
+
+        public ObjectDictionarySerializer(DictionaryRepresentation dictionaryRepresentation, IBsonSerializer keySerializer, IBsonSerializer valueSerializer)
+            : base(dictionaryRepresentation, keySerializer, valueSerializer)
+        {
+        }
+
+        protected override Dictionary<string, object> CreateInstance()
+        {
+            return new Dictionary<string, object>();
+        }
+    }
+}
---
+++ src/Jane.MongoDb/Serializers/SimpleObjectSerializer.cs
@@ -0,0 +1,123 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using System;
+using System.Reflection;
+
+namespace Jane.MongoDb.Serializers
+{
+    public class SimpleObjectSerializer : ObjectSerializer
+    {
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, object value)
+        {
+            var bsonWriter = context.Writer;
+
+            if (value == null)
+            {
+                bsonWriter.WriteNull();
+            }
+            else
+            {
+                var actualType = value.GetType();
+                if (actualType == typeof(object))
+                {
+                    bsonWriter.WriteStartDocument();
+                    bsonWriter.WriteEndDocument();
+                }
+                else
+                {
+                    // certain types can be written directly as BSON value
+                    // if we're not at the top level document, or if we're using the JsonWriter
+                    if (bsonWriter.State == BsonWriterState.Value || bsonWriter is JsonWriter)
+                    {
+                        if (actualType.GetTypeInfo().IsEnum)
+                        {
+                            bsonWriter.WriteString(value.ToString());
+                            return;
+                        }
+
+                        switch (Type.GetTypeCode(actualType))
+                        {
+                            case TypeCode.Boolean:
+                                bsonWriter.WriteBoolean((bool)value);
+                                return;
+
+                            case TypeCode.DateTime:
+                                // TODO: is this right? will lose precision after round trip
+                                var bsonDateTime = new BsonDateTime(BsonUtils.ToUniversalTime((DateTime)value));
+                                bsonWriter.WriteDateTime(bsonDateTime.MillisecondsSinceEpoch);
+                                return;
+
+                            case TypeCode.Decimal:
+                                bsonWriter.WriteDecimal128(Decimal128.Parse(value.ToString()));
+                                return;
+
+                            case TypeCode.Double:
+                                bsonWriter.WriteDouble((double)value);
+                                return;
+
+                            case TypeCode.Int16:
+                                // TODO: is this right? will change type to Int32 after round trip
+                                bsonWriter.WriteInt32((short)value);
+                                return;
+
+                            case TypeCode.Int32:
+                                bsonWriter.WriteInt32((int)value);
+                                return;
+
+                            case TypeCode.Int64:
+                                bsonWriter.WriteInt64((long)value);
+                                return;
+
+                            case TypeCode.Object:
+                                if (actualType == typeof(Decimal128))
+                                {
+                                    var decimal128 = (Decimal128)value;
+                                    bsonWriter.WriteDecimal128(decimal128);
+                                    return;
+                                }
+                                if (actualType == typeof(Guid))
+                                {
+                                    var guid = (Guid)value;
+                                    var guidRepresentation = bsonWriter.Settings.GuidRepresentation;
+                                    var binaryData = new BsonBinaryData(guid, guidRepresentation);
+                                    bsonWriter.WriteBinaryData(binaryData);
+                                    return;
+                                }
+                                if (actualType == typeof(ObjectId))
+                                {
+                                    bsonWriter.WriteObjectId((ObjectId)value);
+                                    return;
+                                }
+                                break;
+
+                            case TypeCode.String:
+                                bsonWriter.WriteString((string)value);
+                                return;
+                        }
+                    }
+
+                    SerializeDiscriminatedValue(context, args, value, actualType);
+
+                    //throw new NotSupportedException($"serialize bson meet not support type[{actualType.Name}]");
+                }
+            }
+        }
+
+        private void SerializeDiscriminatedValue(BsonSerializationContext context, BsonSerializationArgs args, object value, Type actualType)
+        {
+            var serializer = BsonSerializer.LookupSerializer(actualType);
+
+            var polymorphicSerializer = serializer as IBsonPolymorphicSerializer;
+            if (polymorphicSerializer != null && polymorphicSerializer.IsDiscriminatorCompatibleWithObjectSerializer)
+            {
+                serializer.Serialize(context, args, value);
+            }
+            else
+            {
+                serializer.Serialize(context, value);
+            }
+        }
+    }
+}
