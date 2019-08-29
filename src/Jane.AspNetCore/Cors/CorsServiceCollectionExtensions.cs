a63c87882112ccb32d0b8fcddacfa4913ca6796d
Caskia <liubai61@163.com>
2019-08-29 11:41:06 +0800

    cors allow wildcard subdomain

--- src/Jane.AspNetCore/Cors/CorsServiceCollectionExtensions.cs
+++ src/Jane.AspNetCore/Cors/CorsServiceCollectionExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using Jane.Extensions;
-using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
 using System.Linq;
 
@@ -12,13 +10,13 @@ namespace Jane.AspNetCore.Cors
     {
         public static void AddCorsPolicy(this IServiceCollection services, IConfigurationRoot appConfiguration)
         {
-            services.TryAdd(ServiceDescriptor.Transient<ICorsService, WildCardCorsService>());
             services.AddCors(options =>
             {
                 options.AddPolicy(CorsPolicyNames.DefaultCorsPolicyName, builder =>
                 {
                     //App:CorsOrigins in appsettings.json can contain more than one address with splitted by comma.
                     builder
+                        .SetIsOriginAllowedToAllowWildcardSubdomains()
                         .WithOrigins(appConfiguration["App:CorsOrigins"].Split(",", StringSplitOptions.RemoveEmptyEntries).Select(o => o.RemovePostFix("/")).ToArray())
                         .AllowAnyHeader()
                         .AllowAnyMethod()
