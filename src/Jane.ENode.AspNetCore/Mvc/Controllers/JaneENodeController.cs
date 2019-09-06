8adb8db33e46fe6e4762ed4ec12bdb8fba3d4d7d
Caskia <liubai61@163.com>
2019-09-06 14:41:27 +0800

    add command service extensions

--- src/Jane.ENode.AspNetCore/Mvc/Controllers/JaneENodeController.cs
+++ src/Jane.ENode.AspNetCore/Mvc/Controllers/JaneENodeController.cs
@@ -6,6 +6,7 @@ using Jane.AspNetCore.Mvc.Controllers;
 using System.Threading.Tasks;
 using System;
 using Newtonsoft.Json;
+using Jane.ENode;
 
 namespace Jane.ENode.AspNetCore.Mvc.Controllers
 {
@@ -82,70 +83,14 @@ namespace Jane.ENode.AspNetCore.Mvc.Controllers
 
         #endregion Authorize Properties
 
-        protected async Task<AsyncTaskResult<CommandResult>> ExecuteCommandAsync(ICommand command, CommandReturnType commandReturnType = CommandReturnType.CommandExecuted, int millisecondsDelay = 10000, bool autoProcessResultException = true)
+        protected Task<AsyncTaskResult<CommandResult>> ExecuteCommandAsync(ICommand command, CommandReturnType commandReturnType = CommandReturnType.CommandExecuted, int millisecondsDelay = 10000, bool autoProcessResultException = true)
         {
-            var result = await _commandService.ExecuteAsync(command, commandReturnType).TimeoutAfter(millisecondsDelay);
-            if (autoProcessResultException)
-            {
-                ProcessExecuteCommandAsyncResultException(result);
-            }
-            return result;
+            return _commandService.ExecuteCommandAsync(command, commandReturnType, millisecondsDelay, autoProcessResultException);
         }
 
-        protected void ProcessExecuteCommandAsyncResultException(AsyncTaskResult<CommandResult> result)
+        protected Task<AsyncTaskResult> SendCommandAsync(ICommand command, bool autoProcessResultException = true)
         {
-            if (result.Status != AsyncTaskStatus.Success)
-            {
-                throw new UserFriendlyException("internal network meets problems!", result.ErrorMessage);
-            }
-
-            switch (result.Data.Status)
-            {
-                case CommandStatus.Success:
-                    break;
-
-                case CommandStatus.NothingChanged:
-                    break;
-
-                case CommandStatus.None:
-                case CommandStatus.Failed:
-                default:
-                    var aggregateExceptionPrefix = "One or more errors occurred. (";
-                    var aggregateExceptionSuffix = ")";
-                    if (result.Data.ResultType == typeof(JaneSerializableException).Name)
-                    {
-                        var exceptionWrapper = SerializableExceptionWrapper.Deserialize(result.Data.Result);
-                        throw new UserFriendlyException(exceptionWrapper.Code, exceptionWrapper.Message);
-                    }
-                    else if (result.Data.ResultType == typeof(string).FullName && result.Data.Result.StartsWith(aggregateExceptionPrefix) && result.Data.Result.EndsWith(aggregateExceptionSuffix))
-                    {
-                        var exceptionResult = result.Data.Result.Substring(aggregateExceptionPrefix.Length, result.Data.Result.Length - aggregateExceptionPrefix.Length - aggregateExceptionSuffix.Length);
-                        var exceptionWrapper = SerializableExceptionWrapper.Deserialize(exceptionResult);
-                        throw new UserFriendlyException(exceptionWrapper.Code, exceptionWrapper.Message);
-                    }
-                    else
-                    {
-                        throw new UserFriendlyException(result.Data.Result);
-                    }
-            }
-        }
-
-        protected void ProcessSendCommandAsyncResultException(AsyncTaskResult result)
-        {
-            if (result.Status != AsyncTaskStatus.Success)
-            {
-                throw new UserFriendlyException("internal network meets problems!", result.ErrorMessage);
-            }
-        }
-
-        protected async Task<AsyncTaskResult> SendCommandAsync(ICommand command, bool autoProcessResultException = true)
-        {
-            var result = await _commandService.SendAsync(command);
-            if (autoProcessResultException)
-            {
-                ProcessSendCommandAsyncResultException(result);
-            }
-            return result;
+            return _commandService.SendCommandAsync(command, autoProcessResultException);
         }
     }
 }
---
+++ src/Jane.ENode/CommandServiceExtensions.cs
@@ -0,0 +1,78 @@
+﻿using ECommon.Extensions;
+using ECommon.IO;
+using ENode.Commanding;
+using System.Threading.Tasks;
+
+namespace Jane.ENode
+{
+    public static class CommandServiceExtensions
+    {
+        public static async Task<AsyncTaskResult<CommandResult>> ExecuteCommandAsync(this ICommandService commandService, ICommand command, CommandReturnType commandReturnType = CommandReturnType.CommandExecuted, int millisecondsDelay = 10000, bool autoProcessResultException = true)
+        {
+            var result = await commandService.ExecuteAsync(command, commandReturnType).TimeoutAfter(millisecondsDelay);
+            if (autoProcessResultException)
+            {
+                ProcessExecuteCommandAsyncResultException(result);
+            }
+
+            return result;
+        }
+
+        public static async Task<AsyncTaskResult> SendCommandAsync(this ICommandService commandService, ICommand command, bool autoProcessResultException = true)
+        {
+            var result = await commandService.SendAsync(command);
+            if (autoProcessResultException)
+            {
+                ProcessSendCommandAsyncResultException(result);
+            }
+
+            return result;
+        }
+
+        private static void ProcessExecuteCommandAsyncResultException(AsyncTaskResult<CommandResult> result)
+        {
+            if (result.Status != AsyncTaskStatus.Success)
+            {
+                throw new UserFriendlyException("internal network meets problems!", result.ErrorMessage);
+            }
+
+            switch (result.Data.Status)
+            {
+                case CommandStatus.Success:
+                    break;
+
+                case CommandStatus.NothingChanged:
+                    break;
+
+                case CommandStatus.None:
+                case CommandStatus.Failed:
+                default:
+                    var aggregateExceptionPrefix = "One or more errors occurred. (";
+                    var aggregateExceptionSuffix = ")";
+                    if (result.Data.ResultType == typeof(JaneSerializableException).Name)
+                    {
+                        var exceptionWrapper = SerializableExceptionWrapper.Deserialize(result.Data.Result);
+                        throw new UserFriendlyException(exceptionWrapper.Code, exceptionWrapper.Message);
+                    }
+                    else if (result.Data.ResultType == typeof(string).FullName && result.Data.Result.StartsWith(aggregateExceptionPrefix) && result.Data.Result.EndsWith(aggregateExceptionSuffix))
+                    {
+                        var exceptionResult = result.Data.Result.Substring(aggregateExceptionPrefix.Length, result.Data.Result.Length - aggregateExceptionPrefix.Length - aggregateExceptionSuffix.Length);
+                        var exceptionWrapper = SerializableExceptionWrapper.Deserialize(exceptionResult);
+                        throw new UserFriendlyException(exceptionWrapper.Code, exceptionWrapper.Message);
+                    }
+                    else
+                    {
+                        throw new UserFriendlyException(result.Data.Result);
+                    }
+            }
+        }
+
+        private static void ProcessSendCommandAsyncResultException(AsyncTaskResult result)
+        {
+            if (result.Status != AsyncTaskStatus.Success)
+            {
+                throw new UserFriendlyException("internal network meets problems!", result.ErrorMessage);
+            }
+        }
+    }
+}
