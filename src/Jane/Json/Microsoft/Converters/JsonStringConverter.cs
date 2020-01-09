107d9793413e5d18bb4deec156f9ee6ded10118a
Caskia <liubai61@163.com>
2020-01-09 16:46:01 +0800

    combine json boolean and json number converter

--- src/Jane/Json/Microsoft/Converters/JsonStringConverter.cs
+++ src/Jane/Json/Microsoft/Converters/JsonStringConverter.cs
@@ -4,13 +4,26 @@ using System.Text.Json.Serialization;
 
 namespace Jane.Json.Microsoft
 {
-    public class JsonBooleanStringConverter : JsonConverter<string>
+    public class JsonStringConverter : JsonConverter<string>
     {
         public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
+            if (reader.TokenType == JsonTokenType.Number)
+            {
+                if (reader.TryGetInt32(out int number))
+                {
+                    return number.ToString();
+                }
+
+                if (reader.TryGetDouble(out double dNumber))
+                {
+                    return dNumber.ToString();
+                }
+            }
+
             if (reader.TokenType == JsonTokenType.True || reader.TokenType == JsonTokenType.False)
             {
-                return reader.GetBoolean().ToString().ToLower();
+                return reader.GetBoolean().ToString();
             }
 
             return reader.GetString();
