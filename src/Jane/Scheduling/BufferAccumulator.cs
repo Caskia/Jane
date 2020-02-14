2ae38b1e2c93951bec52866759744d2be90a6c7a
Caskia <liubai61@163.com>
2020-02-14 18:19:51 +0800

    add accumulator

---
+++ src/Jane/Scheduling/BufferAccumulator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+
+namespace Jane.Scheduling
+{
+    public class BufferAccumulator<T>
+    {
+        private int _count;
+        private TimeSpan _interval;
+        private ConcurrentDictionary<string, ISubject<T>> _maps = new ConcurrentDictionary<string, ISubject<T>>();
+        private Func<IList<T>, Task> _work;
+
+        public BufferAccumulator(Func<IList<T>, Task> work, TimeSpan interval, int count)
+        {
+            if (work == null)
+            {
+                throw new ArgumentNullException(nameof(work));
+            }
+
+            if (interval == null)
+            {
+                throw new ArgumentNullException(nameof(interval));
+            }
+
+            _work = work;
+            _interval = interval;
+            _count = count;
+        }
+
+        public void Enqueue(string key, T element)
+        {
+            var queue = _maps.GetOrAdd(key, x =>
+            {
+                var q = Subject.Synchronize(new Subject<T>());
+
+                q.Buffer(_interval, _count)
+                .Select(e => Observable.FromAsync(() => _work(e)))
+                .Concat()
+                .Subscribe();
+
+                return q;
+            });
+
+            queue.OnNext(element);
+        }
+    }
+}
---
+++ src/Jane/Scheduling/SampleAccumulator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+
+namespace Jane.Scheduling
+{
+    public class SampleAccumulator<T>
+    {
+        private TimeSpan _interval;
+        private ConcurrentDictionary<string, ISubject<T>> _maps = new ConcurrentDictionary<string, ISubject<T>>();
+        private Func<T, Task> _work;
+
+        public SampleAccumulator(Func<T, Task> work, TimeSpan interval)
+        {
+            if (work == null)
+            {
+                throw new ArgumentNullException(nameof(work));
+            }
+
+            if (interval == null)
+            {
+                throw new ArgumentNullException(nameof(interval));
+            }
+
+            _interval = interval;
+            _work = work;
+        }
+
+        public void Enqueue(string key, T element)
+        {
+            var queue = _maps.GetOrAdd(key, x =>
+            {
+                var q = Subject.Synchronize(new Subject<T>());
+
+                q.Sample(_interval)
+                .Select(e => Observable.FromAsync(() => _work(e)))
+                .Concat()
+                .Subscribe();
+
+                return q;
+            });
+
+            queue.OnNext(element);
+        }
+    }
+}
