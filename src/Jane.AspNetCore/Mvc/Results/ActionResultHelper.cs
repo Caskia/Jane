d66aaa39a5cbbcba420b72341ac9d3c6026d3f03
Caskia <liubai61@163.com>
2020-04-08 12:08:00 +0800

    action result

--- src/Jane.AspNetCore/Mvc/Results/ActionResultHelper.cs
+++ src/Jane.AspNetCore/Mvc/Results/ActionResultHelper.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,6 +9,18 @@ namespace Jane.AspNetCore.Mvc.Results
 {
     public static class ActionResultHelper
     {
+        static ActionResultHelper()
+        {
+            ObjectResultTypes = new List<Type>
+            {
+                typeof(JsonResult),
+                typeof(ObjectResult),
+                typeof(NoContentResult)
+            };
+        }
+
+        public static List<Type> ObjectResultTypes { get; }
+
         public static bool IsObjectResult(Type returnType)
         {
             //Get the actual return type (unwrap Task)
@@ -19,17 +33,12 @@ namespace Jane.AspNetCore.Mvc.Results
                 returnType = returnType.GenericTypeArguments[0];
             }
 
-            if (typeof(IActionResult).GetTypeInfo().IsAssignableFrom(returnType))
+            if (!typeof(IActionResult).IsAssignableFrom(returnType))
             {
-                if (typeof(JsonResult).GetTypeInfo().IsAssignableFrom(returnType) || typeof(ObjectResult).GetTypeInfo().IsAssignableFrom(returnType))
-                {
-                    return true;
-                }
-
-                return false;
+                return true;
             }
 
-            return true;
+            return ObjectResultTypes.Any(t => t.IsAssignableFrom(returnType));
         }
     }
 }
