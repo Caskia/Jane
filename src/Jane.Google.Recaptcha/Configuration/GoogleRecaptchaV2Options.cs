d134bab749106c7b41cc9a9f50ce1bd2832a984a
Caskia <liubai61@163.com>
2023-07-19 19:42:26 +0800

    google recaptchav2

---
+++ src/Jane.Google.Recaptcha/Configuration/GoogleRecaptchaV2Options.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jane.Configurations
+{
+    public class GoogleRecaptchaV3Options
+    {
+        public string AppSecret { get; set; }
+
+        public double Threshold { get; set; }
+    }
+}
+
---
+++ src/Jane.Google.Recaptcha/GoogleRecaptchaV2Service.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+using Jane.Configurations;
+using Jane.Google.Recaptcha;
+using Jane.Google.Recaptcha.Apis;
+using Microsoft.Extensions.Options;
+
+namespace Jane.Captcha
+{
+    public class GoogleRecaptchaV3Service : ICaptchaService
+    {
+        private readonly GoogleRecaptchaV3Options _options;
+        private readonly IGoogleRecaptchaV3Api _googleRecaptchaV3Api;
+
+        public GoogleRecaptchaV3Service(
+            IOptions<GoogleRecaptchaV3Options> optionsAccessor,
+            IGoogleRecaptchaV3Api googleRecaptchaV3Api
+            )
+        {
+            _options = optionsAccessor.Value;
+            _googleRecaptchaV3Api = googleRecaptchaV3Api;
+        }
+
+
+        public async Task<ValidateResult> ValidateAsync(string token)
+        {
+            var response = await _googleRecaptchaV3Api.SiteVerifyAsync(new SiteVerifyV3Request
+            {
+                Secret = _options.AppSecret,
+                Response = token
+            });
+
+            if (response.Success)
+            {
+                return new ValidateResult
+                {
+                    Success = response.Score >= _options.Threshold,
+                    Error = response.Score >= _options.Threshold ? null : $"score[{response.Score}] less than threshold[{_options.Threshold}]."
+                };
+            }
+            else
+            {
+                return new ValidateResult
+                {
+                    Success = response.Success,
+                    Error = string.Join(",", response.ErrorCodes)
+                };
+            }
+        }
+    }
+}
+
