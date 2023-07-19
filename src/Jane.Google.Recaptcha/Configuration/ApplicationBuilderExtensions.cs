713ffb5a0b5586aaf251a29c576725e1672b1a80
Caskia <liubai61@163.com>
2023-07-19 13:06:52 +0800

    register test captcha

---
+++ src/Jane.Google.Recaptcha/Configuration/ApplicationBuilderExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Jane.AspNetCore.ExceptionHandling;
+using Jane.AspNetCore.Logging;
+using Jane.AspNetCore.Middlewares;
+using Jane.Autofac;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Jane.Configurations
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void UseHostNameHeader(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<HostNameMiddleware>();
+        }
+
+        public static void UseJane(this IApplicationBuilder app)
+        {
+            app.ApplicationServices.PopulateJaneDIContainer();
+
+            app.UseJaneLoggerFactory();
+        }
+
+        public static void UseJaneExceptionHandling(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<JaneExceptionHandlingMiddleware>();
+        }
+
+        public static void UseJaneLoggerFactory(this IApplicationBuilder app)
+        {
+            var janeLoggerFactory = app.ApplicationServices.GetService<Jane.Logging.ILoggerFactory>();
+            if (janeLoggerFactory == null)
+            {
+                return;
+            }
+
+            app.ApplicationServices
+                .GetRequiredService<ILoggerFactory>()
+                .AddJaneLogger(janeLoggerFactory);
+        }
+
+        public static void UseProcessingTimeHeader(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ProcessingTimeMiddleware>();
+        }
+    }
+}
