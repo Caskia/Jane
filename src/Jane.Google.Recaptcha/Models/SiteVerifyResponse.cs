b93cd3968999c0b819795aeb28f1dd4464ec9a5b
Caskia <liubai61@163.com>
2023-07-19 18:09:42 +0800

    recaptcha html test

---
+++ src/Jane.Google.Recaptcha/Models/SiteVerifyResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Jane.Google.Recaptcha
+{
+    public class SiteVerifyOutput
+    {
+        public bool Success { get; set; }
+
+        public long ChallengeTs { get; set; }
+
+        public string Hostname { get; set; }
+
+        [JsonPropertyName("error-codes")]
+        public List<string> ErrorCodes { get; set; }
+
+        public float Score { get; set; }
+
+        public string Action { get; set; }
+    }
+}
+
